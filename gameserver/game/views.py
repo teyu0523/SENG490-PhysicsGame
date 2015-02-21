476219e484e14e786c9ba9ff1acc4725c8e4f463
Geoffrey Gollmer <Geoff@Geoffreys-MacBook-Pro.local>
2015-02-20 23:53:14 -0800

    Created answer check and submit endpoints. Not tested at all. Again. :S

--- gameserver/game/views.py
+++ gameserver/game/views.py
@@ -1,4 +1,4 @@
-from django.http import JsonResponse, HttpResponseBadRequest
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponse
 
 from rest_framework.views import APIView
 from rest_framework.authentication import TokenAuthentication
@@ -7,7 +7,7 @@ from rest_framework.permissions import IsAuthenticated
 # from rest_framework.parsers import JSONParser
 
 # from game.mixins import *
-from game.models import Grade, LessonGrade, WeightedLesson, Question
+from game.models import Grade, LessonGrade, WeightedLesson, Question, Answer, CannonsAnswer
 
 
 class StudentListLessons(APIView):
@@ -78,7 +78,7 @@ class StudentLessonDetails(APIView):
         lesson_structure = {}
         lesson_structure['id'] = lesson_grade.id
         lesson_structure['lesson_id'] = lesson_grade.lesson.id
-        lesson_structure['weight'] = WeightedLesson.objects.get(lesson_id=lesson_grade.lesson_id, course_id=grade.course_id).weight
+        lesson_structure['weight'] = WeightedLesson.objects.get(lesson_id=lesson_grade.lesson_id, course_id=lesson_grade.course_grade.course_id).weight
         lesson_structure['lesson_state'] = lesson_grade.get_lesson_state_display()
         lesson_structure['total_questions'] = lesson_grade.get_grades()['total_questions']
         lesson_structure['answered_questions'] = lesson_grade.get_grades()['answered_questions']
@@ -118,5 +118,72 @@ class StudentLessonDetails(APIView):
 
         return JsonResponse(lesson_structure)
 
+
+class StudentAnswer(APIView):
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        (answer, created) = Answer.objects.get_or_create(question_id=request.GET['id'], lesson_grade__course_grade__student_id=request.user.id)
+        if created:
+            if answer.question.question_type == Question.CANNONS:
+                CannonsAnswer.objects.create(answer=answer)
+
+        answer_structure = {}
+        answer_structure['id'] = answer.id
+        answer_structure['question_id'] = answer.question.id
+        answer_structure['grade'] = answer.grade
+        answer_structure['total_tries'] = answer.total_tries
+        if answer.question.question_type == Question.CANNONS:
+            answer_structure['player_tank_pos_x'] = answer.cannons_extension.player_tank_pos_x
+            answer_structure['player_tank_pos_y'] = answer.cannons_extension.player_tank_pos_y
+            answer_structure['player_tank_angle'] = answer.cannons_extension.player_tank_angle
+            answer_structure['player_tank_velocity'] = answer.cannons_extension.player_tank_velocity
+            answer_structure['enemy_tank_pos_x'] = answer.cannons_extension.enemy_tank_pos_x
+            answer_structure['enemy_tank_pos_y'] = answer.cannons_extension.enemy_tank_pos_y
+            answer_structure['enemy_tank_angle'] = answer.cannons_extension.enemy_tank_angle
+            answer_structure['enemy_tank_velocity'] = answer.cannons_extension.enemy_tank_velocity
+
+        return JsonResponse(answer_structure)
+
+    def post(self, request):
+        try:
+            LessonGrade.objects.get(course_grade__student_id=request.user.id, lesson_id=Question.objects.get(request.GET['id']).lesson_id)
+        except:
+            return HttpResponseBadRequest('total_tries is a required field')
+
+        (answer, created) = Answer.objects.get_or_create(question_id=request.GET['id'], lesson_grade__course_grade__student_id=request.user.id)
+        if created:
+            if answer.question.question_type == Question.CANNONS:
+                CannonsAnswer.objects.create(answer=answer)
+
+        if 'total_tries' in request.DATA:
+            answer.total_tries = request.DATA['total_tries']
+            answer.calculate_grade()
+        else:
+            return HttpResponseBadRequest('total_tries is a required field')
+        if answer.question.question_type == Question.CANNONS:
+            if 'player_tank_pos_x' in request.DATA:
+                answer.cannons_extension.player_tank_pos_x = request.DATA['player_tank_pos_x']
+            if 'player_tank_pos_y' in request.DATA:
+                answer.cannons_extension.player_tank_pos_y = request.DATA['player_tank_pos_y']
+            if 'player_tank_angle' in request.DATA:
+                answer.cannons_extension.player_tank_angle = request.DATA['player_tank_angle']
+            if 'player_tank_velocity' in request.DATA:
+                answer.cannons_extension.player_tank_velocity = request.DATA['player_tank_velocity']
+            if 'enemy_tank_pos_x' in request.DATA:
+                answer.cannons_extension.enemy_tank_pos_x = request.DATA['enemy_tank_pos_x']
+            if 'enemy_tank_pos_y' in request.DATA:
+                answer.cannons_extension.enemy_tank_pos_y = request.DATA['enemy_tank_pos_y']
+            if 'enemy_tank_angle' in request.DATA:
+                answer.cannons_extension.enemy_tank_angle = request.DATA['enemy_tank_angle']
+            if 'enemy_tank_velocity' in request.DATA:
+                answer.cannons_extension.enemy_tank_velocity = request.DATA['enemy_tank_velocity']
+            answer.cannons_extension.save()
+        answer.save()
+
+        return HttpResponse()
+
 student_list_lessons = StudentListLessons.as_view()
 student_lesson_details = StudentLessonDetails.as_view()
+student_answer = StudentAnswer.as_view()

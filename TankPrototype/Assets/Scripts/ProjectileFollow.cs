e74dfdf1b8df06c9a6aad93741ec66f9f805be14
Mikko Sanchez <mikkosanchez@gmail.com>
2015-02-26 14:46:23 -0800

    added target, projectile collision, visual effects

--- TankPrototype/Assets/Scripts/ProjectileFollow.cs
+++ TankPrototype/Assets/Scripts/ProjectileFollow.cs
@@ -6,16 +6,37 @@ public class ProjectileFollow : MonoBehaviour {
 	public Transform tank;
 	public Transform farLeft;
 	public Transform farRight;
+	
+	 GameObject targetObject = null;
+
+	void Start(){
+		targetObject = tank.gameObject;
+	}
+
+	public IEnumerator resetToTank() {
+		yield return new WaitForSeconds(1f);
+		targetObject = tank.gameObject;
+	}
 
 
 	void Update () {
 		Vector3 newPosition = transform.position;
-		if (GameObject.Find("Asteroid") == null) {
-			newPosition.x = tank.position.x;
+		if (targetObject != null) {
+			newPosition.x = targetObject.transform.position.x;
+		} 
+		else {
+			StartCoroutine(resetToTank());
 		}
-		else{
-			newPosition.x = GameObject.Find("Asteroid").transform.position.x;
+		if (GameObject.FindWithTag ("Bullet") != null) {
+			targetObject = GameObject.FindWithTag("Bullet");
 		}
+
+		//if (GameObject.FindWithTag("Bullet") == null) {
+			//newPosition.x = tank.position.x;
+		//}
+		//else{
+			//newPosition.x = GameObject.FindWithTag("Bullet").transform.position.x;
+		//}
 		newPosition.x = Mathf.Clamp (newPosition.x, farLeft.position.x, farRight.position.x);
 		transform.position = newPosition;
 	}

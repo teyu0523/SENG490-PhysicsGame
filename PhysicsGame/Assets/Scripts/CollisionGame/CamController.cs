e2f91407d6e248f3f64b927c4488035c57366432
Teyu Chang <teyu0523@gmail.com>
2015-03-17 22:17:30 -0700

    Added screen smooth control with mouse click

--- PhysicsGame/Assets/Scripts/CollisionGame/CamController.cs
+++ PhysicsGame/Assets/Scripts/CollisionGame/CamController.cs
@@ -3,18 +3,67 @@ using System.Collections;
 
 public class CamController : MonoBehaviour {
 
-	private GameObject cam;
+	//private GameObject cam;
 	private float speed;
+	private Vector3 startVec;
+	private bool mousePressed;
+	public float smoothTime = 0.001f;
+	public float MinXPosRestriction;
+	public float MaxXPosRestriction;
+	public float MinYPosRestriction;
+	public float MaxYPosRestriction;
+	
+	private Vector3 velocity = Vector3.zero;
 
 	// Use this for initialization
 	void Start () {
-		cam = GameObject.FindWithTag("MainCamera");
+		mousePressed = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		speed = Input.GetAxisRaw("Horizontal");// * Time.deltaTime;
-		Vector3 pos = cam.transform.position;
-		cam.transform.position = Vector3.Lerp(pos,new Vector3(pos.x+speed, pos.y, pos.z),0.1f);
+
+
+		if(Input.GetMouseButtonDown(0)){
+			mousePressed = true;
+		}else if(Input.GetMouseButtonUp(0)){
+			mousePressed = false;
+		}
+		if(mousePressed){
+			transform.position = Vector3.SmoothDamp(
+				transform.position,  
+				transform.position - new Vector3(
+					Input.GetAxis("Mouse X")*10,
+					0, 
+					0), 
+				ref velocity, 
+				smoothTime);
+		}
+		// Need to fix the camera border
+		/*if(Input.GetAxis("Mouse ScrollWheel") > 0){
+		    this.camera.fieldOfView -= 4;
+		} else if(Input.GetAxis("Mouse ScrollWheel") < 0) {
+			this.camera.fieldOfView += 4;
+		}*/
+		speed = Input.GetAxisRaw("Horizontal");
+		transform.position = Vector3.SmoothDamp(
+				transform.position,  
+				new Vector3(
+					transform.position.x+(speed*10), 
+					transform.position.y, 
+					transform.position.z
+				), 
+			ref velocity, 
+			smoothTime);
+
+		transform.position = new Vector3 (
+			Mathf.Clamp(
+				transform.position.x, 
+				MinXPosRestriction, 
+				MaxXPosRestriction
+				), 
+			transform.position.y, 
+			transform.position.z
+			);
 	}
 }

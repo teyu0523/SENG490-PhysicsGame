a8952928c784f2436dc87de2189d56f5ff2e8335
Mikko Sanchez <mikkosanchez@gmail.com>
2015-03-23 12:33:46 -0700

    cleaned up Tank Game assets and ported into PhysicsGame Project. Has issues with loading screen no dissappearing in Tank Game.

---
+++ PhysicsGame/Assets/TankGame/Scripts/TankController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankController : MonoBehaviour {
+	//speed constant for tank and barrel movenment
+	public float speed = 6f;
+	public float velocity = 100f;
+
+	//need a public variable for distance from enemy tank
+	public float currentBarrelAngle;
+
+	public Transform projectileSpawn;
+	public GameObject projectile;
+	public Rigidbody2D barrelRigidbody;
+
+	Vector2 movement;
+	Rigidbody2D tankRigidbody;
+	GameObject bullet;
+
+
+	void Awake(){
+		tankRigidbody = GetComponent<Rigidbody2D> ();
+		
+	}
+
+	void Update(){
+		
+		if(Input.GetButton("Jump")){
+
+			if (bullet == null){
+				bullet = (GameObject) Instantiate(projectile, projectileSpawn.position, projectileSpawn.transform.rotation);
+				bullet.rigidbody2D.velocity = (Quaternion.Euler(0, 0, currentBarrelAngle) * Vector3.right) * velocity;
+			}
+		}
+		VelocityChange ();
+
+	}
+
+	void FixedUpdate () {
+		
+		float h = Input.GetAxisRaw ("Horizontal");
+		float z = Input.GetAxisRaw ("Vertical");
+		Move (h);
+		Rotate (z);
+
+		
+	}
+
+	void VelocityChange (){
+		if(Input.GetKeyDown ("e")){
+			if (velocity < 100f){
+				velocity+= 1;
+			}
+		}
+		if(Input.GetKeyDown ("q")){
+			if (velocity > 10f){
+				velocity-= 1;
+			}
+		}
+	}
+
+
+	//move tank left or right
+	void Move (float h){
+		
+		movement.Set (h, 0f);
+		movement = movement.normalized * speed * Time.deltaTime;
+		tankRigidbody.MovePosition (tankRigidbody.position + movement);
+		
+		
+	}
+
+	//move tank berrel
+	void Rotate(float z){
+		currentBarrelAngle = barrelRigidbody.rotation;
+
+		if (currentBarrelAngle + z < 0) {
+			barrelRigidbody.MoveRotation (0);
+		} else if (currentBarrelAngle + z > 90) {
+			barrelRigidbody.MoveRotation (90);
+		}
+		else{
+			barrelRigidbody.MoveRotation (currentBarrelAngle + z * speed * Time.deltaTime);
+		}
+	}
+
+}
---
+++ TankPrototype/Assets/TankGame/Scripts/Resetter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resetter : MonoBehaviour {
+
+
+	void Start () {
+	}
+
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.R)){
+			Reset();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if (other.rigidbody2D == GameObject.FindWithTag("Bullet").rigidbody2D) {
+			Destroy(GameObject.FindWithTag("Bullet"));
+		}
+	}
+
+	void Reset(){
+		Application.LoadLevel (Application.loadedLevel);
+	}
+}

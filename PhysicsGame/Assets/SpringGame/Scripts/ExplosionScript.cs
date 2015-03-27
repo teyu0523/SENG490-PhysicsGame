c8a3c77383973fef0fe82c6a749b5cbdd8a63685
Adam Anderson <adam_anderson81@hotmail.com>
2015-03-27 12:10:17 -0700

    Fixed sounds in spring game. Made explosion grow. Got rid of empty space at sides of screen.

---
+++ PhysicsGame/Assets/SpringGame/Scripts/ExplosionScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionScript : MonoBehaviour {
+
+	public float m_delay;
+	public AudioSource source;
+	
+	void Start () {
+		transform.localScale = new Vector3(0.0f, 0.0f, 0.0f);
+		StartCoroutine(dieDelayed(m_delay));
+
+		source = GetComponent<AudioSource>();
+		source.Play();
+	}
+	
+	public IEnumerator dieDelayed(float delay) {
+		yield return new WaitForSeconds(delay);
+		Destroy(gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float scale = Time.deltaTime;
+		transform.localScale += new Vector3 (scale, scale, scale);
+	}
+}
--- PhysicsGame/Assets/SpringGame/Scripts/ShipController.cs
+++ PhysicsGame/Assets/SpringGame/Scripts/ShipController.cs
@@ -2,25 +2,18 @@
 using System.Collections;
 
 public class ShipController : MonoBehaviour {
-	public SpriteRenderer spriteRenderer;
-	public AudioSource source1;
-	public AudioSource source2;
+	public AudioSource source;
 
 	void Start(){
-		spriteRenderer = GetComponent<SpriteRenderer> ();
-
-		AudioSource[] sources = GetComponents<AudioSource>();
-		source1 = sources[0];
-		//source2 = sources[1];
+		source = GetComponent<AudioSource>();
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
 		if(col.relativeVelocity.magnitude > 5){
-			source1.Play();
 			col.gameObject.GetComponent<EggController>().breakEgg();
 			(GameController.Instance as SpringGameController).OnFailure();
 		} else {
-			//source2.Play();
+			source.Play();
 			col.gameObject.SetActive(false);
 			(GameController.Instance as SpringGameController).OnSuccess();
 		}

d41b8fd488be635ca5d59182bcade4bbe508d23d
Geoffrey E. Gollmer <Geoff.Gollmer@Gmail.com>
2015-03-27 01:34:51 -0700

    Apparently it no work on windows.

--- PhysicsGame/Assets/Scripts/Utility/OnlyOnMobile.cs
+++ PhysicsGame/Assets/Scripts/Utility/OnlyOnMobile.cs
@@ -5,7 +5,7 @@ public class OnlyOnMobile : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
-#if !(UNITY_IOS || UNITY_ANDROID || UNITY_BLACKBERRY)
+#if !(UNITY_IOS || UNITY_ANDROID || UNITY_BLACKBERRY || UNITY_EDITOR)
 		gameObject.SetActive(false);
 #endif
 	}
